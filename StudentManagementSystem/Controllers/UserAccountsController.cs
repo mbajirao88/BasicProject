bf1f7e9b3cf771114670c7a9248668811d0acfd0
t-Baji <t-Baji@PC390519>
2020-03-17 14:30:18 +0530

    Add project files.

---
+++ StudentManagementSystem/Controllers/UserAccountsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentManagementSystem.Models;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class UserAccountsController : Controller
+    {        
+        public ActionResult Signin()
+        {
+            ViewBag.message = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signin(UserLogin lg)
+        {
+            if(ModelState.IsValid)
+            {
+                using (DBEntities db=new DBEntities())
+                {
+                   UserMaster um= db.UserMasters.Where
+                        (
+                        x => x.Username == lg.Username && x.Password == lg.Password
+                        ).FirstOrDefault();
+                    if(um!=null)
+                    {
+                        //success
+                        return RedirectToAction("Index", "Home");
+
+                    }
+                    else
+                    {
+                        //failure
+                        ViewBag.message = "Incorrect UserId/Password";
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
